385cba84dd399c8db3ecc15084c46fda0e14ba68
Janis Lebron <janislebron@gmail.com>
2020-12-19 16:29:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,5 @@
-require 'pry'
-
 class Artist
-
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   @@song_count = 0
 
@@ -11,17 +8,21 @@ class Artist
     @songs = []
   end
 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
   def add_song(song)
+    @@song_count += 1
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
     @@song_count += 1
+    s = Song.new(name)
+    self.add_song(s)
+    s
   end
 
   def self.song_count
